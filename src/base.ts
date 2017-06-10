3438d8e18ed1720abc82743f666edf802bceb9ab
kirill <best.portfoleo@gmail.com>
2017-06-10 13:58:25 +0500

    Initialize I18n

--- src/base.ts
+++ src/base.ts
@@ -1,8 +1,9 @@
 import * as externalValidator from 'validator';
 import * as _ from 'underscore';
 
-import i18n from '@j.u.p.iter/i18n';
+import I18n from '@j.u.p.iter/i18n';
 import errors from '@j.u.p.iter/errors';
+import JSONParser from '@j.u.p.iter/json-parser';
 
 import ValidatorExtender from './ValidatorExtender';
 import validatorExtension from './validatorExtension';
@@ -15,6 +16,10 @@ import {
   Schema
 } from './types';
 
+const i18n = new I18n({
+  currentLocale: 'en',
+  jsonParser: new JSONParser()
+});
 
 class SchemaValidator implements SchemaValidatorInterface {
 
@@ -42,7 +47,7 @@ class SchemaValidator implements SchemaValidatorInterface {
             { required } = rules;
 
       if (method !== 'PUT' && required && this._controller.isEmpty(currentValue)) {
-        this._errors.push(new Error('Some Error Message!'));
+        this._errors.push(new Error(i18n.t('validationError')));
       }
     });
 
